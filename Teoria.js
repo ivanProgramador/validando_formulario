5f115c7631b5589c27cff08a32337a37758ccf37
ivan <ivan-guita@hotmail.com>
2024-04-10 11:46:17 -0300

    autenticação funcionado com flash-session

---
+++ Teoria.js
@@ -0,0 +1,7 @@
+/*
+
+Flash sessions são sessões que só duram uma requisição uma das utilidades,
+dela é passar mensagens de aviso   
+
+
+*/
--- index.js
+++ index.js
@@ -26,7 +26,20 @@ app.use(flash());
 
 //rota para o formulario
 app.get("/",(req,res)=>{
-    res.render('index');
+    
+    var emailError  = req.flash("emailError");
+    var nomeError   = req.flash("nomeError");
+    var pontosError = req.flash("pontosError");
+
+     //a req.flash quando usada retorna um array 
+    //então eu vou usar um oprador ternario pra dizer que se ela for vazia 
+    //não vai retonar nada porque o cliente fez o preechimento certo 
+    
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined: emailError;
+    nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined: nomeError;
+    pontosError = (pontosError == undefined || pontosError.length == 0) ? undefined: pontosError;
+
+    res.render('index',{emailError,nomeError,pontosError});
 });
 
 //rota para a validação
@@ -35,9 +48,10 @@ app.post("/form",(req,res)=>{
 
     var {email,nome,pontos} = req.body;
 
-    var emailError;
-    var nomeError;
     var pontosError;
+    var nomeError;
+    var emailError; 
+
 
     if(email == undefined || email =="" ){
         emailError = "email invalido"
@@ -50,6 +64,22 @@ app.post("/form",(req,res)=>{
     }
 
     if(emailError != undefined || nomeError != undefined || pontosError != undefined){
+      /*
+        As flash sessions são sessões que so duram uma requisição normlemente elas são usadas,
+        para disparar mensagens de aviso digamos os cliente esqueceu de digitar a senha ela faz
+        a requisição pra rota percebe o erro e retorna para o formulario com a mensagem de aviso,
+        para o cliente saber que ele voltou para o formulario porque ele esqueceu de digitar a senha.
+
+        A função flash recebe 2 parametros 1 o nome dela e 2 qqual ea varivel que el vai mostrar o valor 
+        caso ela seja chamada 
+       */
+
+        req.flash("emailError", emailError);
+        req.flash("pontosError", pontosError);
+        req.flash("nomeError",nomeError);
+
+
+
       res.redirect("/");
     }else{
         res.send("form completo ");
